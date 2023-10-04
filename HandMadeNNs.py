e79cb75d413c36a84d7b8f6da607797d5c776311
ProgerDreamer <olegodinsikov0224@gmail.com>
2023-10-04 21:03:22 +0300

    one layer classifier

--- HandMadeNNs.py
+++ HandMadeNNs.py
@@ -6,15 +6,18 @@ from util import softmax
 class PerceptronClassifier:
 
     def __init__(self, input_dim, num_classes, lr=1.e-1):
-        self.w = np.random.normal((input_dim, num_classes))
+        self.w = np.random.normal(size=(input_dim, num_classes))
         self.b = np.zeros(num_classes)
+        self.input_dim = input_dim
+        self.num_classes = num_classes
         self.lr = lr
 
     def forward(self, x):
         return softmax(np.dot(x, self.w) + self.b)
 
     def backward(self, x, y_true_oh):
-        self.w += x.reshape(-1, 1) * (self.w * y_true_oh) * self.lr
+        d_w = x.reshape(-1, 1) * np.tile(y_true_oh, (self.input_dim, 1)) * self.lr
+        self.w += d_w
 
     def predict(self, x):
         return np.argmax(np.dot(x, self.w) + self.b)
